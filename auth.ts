512f5774bcd3106993d064d9694a024142f7ffa5
Jacob <jacobschwantes@outlook.com>
2025-02-08 15:28:47 -0600

    init auth

--- auth.ts
+++ auth.ts
@@ -1,6 +1,13 @@
 import NextAuth from "next-auth";
 import Spotify from "next-auth/providers/spotify";
+import { Pool } from "@neondatabase/serverless";
+import PostgresAdapter from "@auth/pg-adapter";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-	providers: [Spotify],
+export const { handlers, signIn, signOut, auth } = NextAuth(() => {
+	const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+	return {
+		secret: process.env.AUTH_SECRET,
+		providers: [Spotify],
+		adapter: PostgresAdapter(pool),
+	};
 });
