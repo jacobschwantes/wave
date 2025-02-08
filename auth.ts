c5327e60e400da74b30e4b17778289dc577273bf
Jacob <jacobschwantes@outlook.com>
2025-02-08 16:10:57 -0600

    auth scopes

--- auth.ts
+++ auth.ts
@@ -3,11 +3,28 @@ import Spotify from "next-auth/providers/spotify";
 import { Pool } from "@neondatabase/serverless";
 import PostgresAdapter from "@auth/pg-adapter";
 
+const spotifyScopes = [
+	"user-read-email",
+	"user-read-private",
+	"playlist-read-private",
+	"playlist-read-collaborative",
+	"user-library-read",
+	"user-top-read",
+	"user-read-recently-played",
+	"user-follow-read",
+].join(" ");
+
 export const { handlers, signIn, signOut, auth } = NextAuth(() => {
 	const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 	return {
 		secret: process.env.AUTH_SECRET,
-		providers: [Spotify],
+		providers: [
+			Spotify({
+				clientId: process.env.AUTH_SPOTIFY_ID,
+				clientSecret: process.env.AUTH_SPOTIFY_SECRET,
+				authorization: `https://accounts.spotify.com/authorize?scope=${spotifyScopes}`,
+			}),
+		],
 		adapter: PostgresAdapter(pool),
 	};
 });
